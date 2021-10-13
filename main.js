ccaed87a7a8cdaa7946aceb4ba9a249b020c532b
Kirill Metrik <kirillmetrik@gmail.com>
2021-10-13 20:30:55 +0200

    Homework 2

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
-/*this is main*/
+const subZero = {
+    name: 'Sub Zero',
+    hp: 100,
+    img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
+    weapon: ['gun', 'knife', 'snowball'],
+    attack: () => {
+        console.log(this.name + ' Fight...');
+    }
+}
+
+const scorpion = {
+    name: 'Scorpion',
+    hp: 100,
+    img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
+    weapon: ['gun', 'knife'],
+    attack: () => {
+        console.log(this.name + ' Fight...');
+    }
+}
+
+function createPlayer(playerClass, playerName, life) {
+    const el = document.createElement('div');
+    el.className = playerClass;
+    el.innerHTML = `
+    <div class="progressbar">
+        <div class="life" style="width: ${life}%"></div>
+        <div class="name">${playerName}</div>
+    </div>
+    <div class="character">
+        <img src="http://reactmarathon-api.herokuapp.com/assets/scorpion.gif" />
+    </div>
+    `;
+
+    const arenas = document.querySelector('div.arenas');
+    arenas.appendChild(el);
+}
+
+createPlayer('player1', 'SCORPION', 50);
+createPlayer('player2', 'SUB-ZERO', 80);
